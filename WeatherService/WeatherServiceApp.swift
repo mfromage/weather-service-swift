ed99cab857f946439446e4b0987b1b05fb1a4473
Michael Michael <hallo@michaelfromage.com>
2024-11-11 23:40:59 +0100

    Project setup

---
+++ WeatherService/WeatherServiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherServiceApp.swift
+//  WeatherService
+//
+//  Created by Michael Michael on 11.11.24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherServiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
